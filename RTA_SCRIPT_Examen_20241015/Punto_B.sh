a886baa0e12ed4c00a577d524a768d09bc2b3efe
DepressiveCat <monstersapbe@gmail.com>
2024-10-16 02:39:31 +0000

    fix: segundo arreglo Punto_B

--- RTA_SCRIPT_Examen_20241015/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_B.sh
@@ -23,7 +23,7 @@ EOF
 
 for i in {5..11}; do
         echo -e "n\n+1G\nW" | sudo fdisk /dev/sdc
-
+done
 #formateamos cada uno de las particiones con extension4
 
 for i in {1..3}; do
@@ -36,16 +36,16 @@ done
 
 #montamos cada uno
 
-sudo mount /dev/sdc1 /Examenes-UTN/alumno_1/parcial_1
-sudo mount /dev/sdc2 /Examenes-UTN/alumno_1/parcial_2
-sudo mount /dev/sdc3 /Examenes-UTN/alumno_1/parcial_3
-sudo mount /dev/sdc5 /Examenes-UTN/alumno_2/parcial_1
-sudo mount /dev/sdc6 /Examenes-UTN/alumno_2/parcial_2
-sudo mount /dev/sdc7 /Examenes-UTN/alumno_2/parcial_3
-sudo mount /dev/sdc8 /Examenes-UTN/alumno_3/parcial_1
-sudo mount /dev/sdc9 /Examenes-UTN/alumno_3/parcial_2
-sudo mount /dev/sdc10 /Examenes-UTN/alumno_3/parcial_3
-sudo mount /dev/sdc11 /Examenes-UTN/profesores
+sudo mount /dev/sdc1 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_1/parcial_1
+sudo mount /dev/sdc2 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_1/parcial_2
+sudo mount /dev/sdc3 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_1/parcial_3
+sudo mount /dev/sdc5 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_2/parcial_1
+sudo mount /dev/sdc6 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_2/parcial_2
+sudo mount /dev/sdc7 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_2/parcial_3
+sudo mount /dev/sdc8 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_3/parcial_1
+sudo mount /dev/sdc9 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_3/parcial_2
+sudo mount /dev/sdc10 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_3/parcial_3
+sudo mount /dev/sdc11 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/profesores
 
 #antes de modificar el archivo hacemos una copia de seguridad
 sudo cp /etc/fstab /etc/fstab.bak
@@ -53,16 +53,16 @@ sudo cp /etc/fstab /etc/fstab.bak
 #modificamos el archivo fstab
 
 sudo tee -a /etc/fstab << END
-UUID=8023d873-0203-40a2-af24-ce383ac9248b /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0
-UUID=9bc5c218-b3e5-4a7a-82db-e31c2575eb24 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0
-UUID=3e085bd7-bfc3-41e8-8513-0124421bd0fa /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0
-UUID=595f3b0d-d381-47c3-9b41-c95bc0719b75 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0
-UUID=acb0ca32-fbfa-441d-bd4a-2b275ec656a8 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0
-UUID=d94f8ba5-1d51-417f-9bf8-63f392520411 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0
-UUID=82e84320-2299-4c26-9f63-316698c99df0 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0
-UUID=e1f09e3f-b085-478c-9f45-14d044e0c4e8 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0
-UUID=6ee233ce-5de1-4f21-9888-e9ec0ce33f7d /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0
-UUID=795d9342-1741-4444-b372-e9f69d4732dd /Examenes-UTN/profesores ext4 defaults 0 0
+UUID=8023d873-0203-40a2-af24-ce383ac9248b /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0
+UUID=9bc5c218-b3e5-4a7a-82db-e31c2575eb24 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0
+UUID=3e085bd7-bfc3-41e8-8513-0124421bd0fa /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0
+UUID=595f3b0d-d381-47c3-9b41-c95bc0719b75 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0
+UUID=acb0ca32-fbfa-441d-bd4a-2b275ec656a8 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0
+UUID=d94f8ba5-1d51-417f-9bf8-63f392520411 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0
+UUID=82e84320-2299-4c26-9f63-316698c99df0 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0
+UUID=e1f09e3f-b085-478c-9f45-14d044e0c4e8 /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0
+UUID=6ee233ce-5de1-4f21-9888-e9ec0ce33f7d /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0
+UUID=795d9342-1741-4444-b372-e9f69d4732dd /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Toscano/RTA_SCRIPT_Examen_20241015/Examenes-UTN/profesores ext4 defaults 0 0
 END
 
 #montamos todos los discos
