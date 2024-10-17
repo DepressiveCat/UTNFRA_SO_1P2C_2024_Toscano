a91f933d0865d8655951f93eb29e3a4354a7c07b
DepressiveCat <monstersapbe@gmail.com>
2024-10-17 01:12:36 +0000

    fix: primer arreglo Punto_C

--- RTA_SCRIPT_Examen_20241015/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241015/Punto_C.sh
@@ -10,9 +10,9 @@ sudo useradd -m -s /bin/bash -G p1c2_2024_gAlumno -p "$(sudo grep vagrant /etc/s
 sudo useradd -m -s /bin/bash -G p1c2_2024_gProfesores -p "$(sudo grep vagrant /etc/shadow | awk -F ':' '{print $2}')" p1c2_2024_P1
 
 #asignar dueño y grupo a su carpeta correspondiente
-sudo chown -R p1c2_2024_A1:p1c2_2024_A1 /Examenes-UTN/alumno_1
-sudo chown -R p1c2_2024_A2:p1c2_2024_A2 /Examenes-UTN/alumno_2
-sudo chown -R p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
+sudo chown -R p1c2_2024_A1:p1c2_2024_gAlumno /Examenes-UTN/alumno_1
+sudo chown -R p1c2_2024_A2:p1c2_2024_gAlumno /Examenes-UTN/alumno_2
+sudo chown -R p1c2_2024_A3:p1c2_2024_gAlumno /Examenes-UTN/alumno_3
 sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
 
 #modificamos permisos 
